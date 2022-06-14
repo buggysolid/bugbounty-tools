12754f76817e66711c40a1f040ebd494b3bba81d
Your Name <you@example.com>
2022-06-14 17:48:41 +0100

    Minimal install script.
    This will install golang, rust and then pull in a bunch of tools from
    projectdiscovery.io along with custom wordlists and SecLists.
    
    This script clones the repo it exists in the idea being that you get the latest
    changes even if you haven't done a git pull recently.
    
    I plan to use snapshots on digitalocean to have an already setup VPS but it is also
    nice to have a kind of live update feature.

---
+++ install.sh
@@ -0,0 +1,19 @@
+echo 'Installing deps.'
+wget 'https://go.dev/dl/go1.18.3.linux-amd64.tar.gz'
+rm -rf /usr/local/go && tar -C /usr/local -xzf go1.18.3.linux-amd64.tar.gz
+curl https://sh.rustup.rs -sSf | sh -s -- -y
+sudo apt-get update && sudo apt-get install -y libpcap-dev git gcc build-essential
+git clone https://github.com/buggysolid/bugbounty-tools
+cp bugbounty-tools/.bashrc $HOME/.bashrc
+source bugbounty-tools/.bashrc
+echo 'Installing tools.'
+go install -v github.com/projectdiscovery/httpx/cmd/httpx@latest
+go install -v github.com/projectdiscovery/dnsx/cmd/dnsx@latest
+go install -v github.com/projectdiscovery/naabu/v2/cmd/naabu@latest
+go install -v github.com/projectdiscovery/subfinder/v2/cmd/subfinder@latest
+go install -v github.com/ffuf/ffuf@latest
+echo "Installing wordlists."
+git clone https://github.com/buggysolid/bugbounty-wordlist
+git clone https://github.com/danielmiessler/SecLists.git
+echo "Run source $HOME/.bashrc for new paths to be loaded into your shell."
+echo "Installed the following tools ffuf, httpx, dnsx, naabu, subfinder."
