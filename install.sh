10c7e7ad80c03c467d33f69d6d87309915cd5c3c
Your Name <you@example.com>
2022-06-19 08:21:35 +0100

    Make file executable and make clone paths relative to home directory.

--- install.sh
+++ install.sh
@@ -3,13 +3,13 @@ wget 'https://go.dev/dl/go1.18.3.linux-amd64.tar.gz'
 rm -rf /usr/local/go && tar -C /usr/local -xzf go1.18.3.linux-amd64.tar.gz
 curl https://sh.rustup.rs -sSf | sh -s -- -y
 sudo apt-get update && sudo apt-get install -y libpcap-dev git gcc build-essential
-git clone https://github.com/codingo/DNSCewl
-cd DNSCewl
+git clone https://github.com/codingo/DNSCewl $HOME/DNSCewl
+cd $HOME/DNSCewl
 make
-cd ..
-git clone https://github.com/buggysolid/bugbounty-tools
+cd $HOME
+git clone https://github.com/buggysolid/bugbounty-tools $HOME/bugbounty-tools
 cp bugbounty-tools/.bashrc $HOME/.bashrc
-source bugbounty-tools/.bashrc
+source $HOME/bugbounty-tools/.bashrc
 echo 'Installing tools.'
 go install -v github.com/projectdiscovery/httpx/cmd/httpx@latest
 go install -v github.com/projectdiscovery/dnsx/cmd/dnsx@latest
@@ -18,7 +18,7 @@ go install -v github.com/projectdiscovery/subfinder/v2/cmd/subfinder@latest
 go install -v github.com/ffuf/ffuf@latest
 go install -v github.com/tomnomnom/hacks/inscope@latest
 echo "Installing wordlists."
-git clone https://github.com/buggysolid/bugbounty-wordlist
-git clone https://github.com/danielmiessler/SecLists.git
+git clone https://github.com/buggysolid/bugbounty-wordlist $HOME/bugbounty-wordlist
+git clone https://github.com/danielmiessler/SecLists.git $HOME/SecLists
 echo "Run source $HOME/.bashrc for new paths to be loaded into your shell."
 echo "Installed the following tools ffuf, httpx, dnsx, naabu, subfinder."
