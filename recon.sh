c10425bf7ef34464242824a1df17cb5bfaf5c073
Ciaran <>
2023-01-21 12:45:13 +0000

    Make random output directory relative to HOME directory.
    The temp directory used to store per scan results was being created in CWD instead of HOME directory.
    
    This was causing scan output to reach the terminal but output sent to the filesystem silently encountered
    an error message and continued.
    
    Considering the rest of the tooling in this scan relies on the previous I will make a note to add fail fast
    mechanisms so errors do not silently propagate.

--- recon.sh
+++ recon.sh
@@ -6,31 +6,31 @@ export CC=/usr/bin/clang
 export EDITOR=vim
 
 gendns(){
-    mksub -silent -df $HOME/$OUTPUT_DIR/subs.txt -w $HOME/bugbounty-wordlist/dns.txt -o $HOME/$OUTPUT_DIR/subs_permutated.txt
+    mksub -silent -df $OUTPUT_DIR/subs.txt -w $HOME/bugbounty-wordlist/dns.txt -o $OUTPUT_DIR/subs_permutated.txt
 }
 
 getdns(){
-    subfinder -d $1 -silent -rL $HOME/bugbounty-wordlist/resolvers.txt -o $HOME/$OUTPUT_DIR/subs.txt
+    subfinder -d $1 -silent -rL $HOME/bugbounty-wordlist/resolvers.txt -o $OUTPUT_DIR/subs.txt
 }
 
 querydns(){
-    dnsx -silent -a -aaaa -cname -retry 1 -r $HOME/bugbounty-wordlist/resolvers.txt -l $HOME/$OUTPUT_DIR/subs_permutated.txt -o $HOME/$OUTPUT_DIR/resolved.txt
-    cat $HOME/$OUTPUT_DIR/resolved.txt | inscope > $HOME/$OUTPUT_DIR/resolved_inscope.txt
+    dnsx -silent -a -aaaa -cname -retry 1 -r $HOME/bugbounty-wordlist/resolvers.txt -l $OUTPUT_DIR/subs_permutated.txt -o $OUTPUT_DIR/resolved.txt
+    cat $OUTPUT_DIR/resolved.txt | inscope > $OUTPUT_DIR/resolved_inscope.txt
     # CNAMEs can end up pointing to something an A record was pointing to in the first place.
-    sort -u $HOME/$OUTPUT_DIR/resolved_inscope.txt -o $HOME/$OUTPUT_DIR/resolved_inscope.txt
+    sort -u $OUTPUT_DIR/resolved_inscope.txt -o $OUTPUT_DIR/resolved_inscope.txt
 }
 
 http(){
-    httpx -silent -retries 0 -timeout 1 -sd -sc -title -mc 404,403,401,301,302,200 -r $HOME/bugbounty-wordlist/resolvers.txt -l $HOME/$OUTPUT_DIR/resolved_inscope.txt -o $HOME/$OUTPUT_DIR/http.txt
+    httpx -silent -retries 0 -timeout 1 -sd -sc -title -mc 404,403,401,301,302,200 -r $HOME/bugbounty-wordlist/resolvers.txt -l $OUTPUT_DIR/resolved_inscope.txt -o $OUTPUT_DIR/http.txt
 }
 
 split_http_results_by_status_code(){
-    awk '/200/' $HOME/$OUTPUT_DIR/http.txt | sort -i -k3 >> $HOME/$OUTPUT_DIR/200.txt
-    awk '/301/' $HOME/$OUTPUT_DIR/http.txt | sort -i -k3 >> $HOME/$OUTPUT_DIR/301.txt
-    awk '/302/' $HOME/$OUTPUT_DIR/http.txt | sort -i -k3 >> $HOME/$OUTPUT_DIR/302.txt
-    awk '/401/' $HOME/$OUTPUT_DIR/http.txt | sort -i -k3 >> $HOME/$OUTPUT_DIR/401.txt
-    awk '/403/' $HOME/$OUTPUT_DIR/http.txt | sort -i -k3 >> $HOME/$OUTPUT_DIR/403.txt
-    awk '/404/' $HOME/$OUTPUT_DIR/http.txt | sort -i -k3 >> $HOME/$OUTPUT_DIR/404.txt
+    awk '/200/' $OUTPUT_DIR/http.txt | sort -i -k3 >> $OUTPUT_DIR/200.txt
+    awk '/301/' $OUTPUT_DIR/http.txt | sort -i -k3 >> $OUTPUT_DIR/301.txt
+    awk '/302/' $OUTPUT_DIR/http.txt | sort -i -k3 >> $OUTPUT_DIR/302.txt
+    awk '/401/' $OUTPUT_DIR/http.txt | sort -i -k3 >> $OUTPUT_DIR/401.txt
+    awk '/403/' $OUTPUT_DIR/http.txt | sort -i -k3 >> $OUTPUT_DIR/403.txt
+    awk '/404/' $OUTPUT_DIR/http.txt | sort -i -k3 >> $OUTPUT_DIR/404.txt
 }
 
 start(){
@@ -45,7 +45,7 @@ start(){
         echo "Usage: recon somedomain.tld wildcard"
         return -1
     fi
-    OUTPUT_DIR="$(mktemp -d -p . $1.XXXXXX)"
+    OUTPUT_DIR="$(mktemp -d -p $HOME/ $1.XXXXXX)"
     getdns $1
     gendns
     querydns
